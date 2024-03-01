b0c5527acc4d0546baca7feb46d79ca2e0c8dce5
seongmin221 <seongmin221@naver.com>
2024-03-01 09:52:38 +0900

    builder pattern

--- builder-pattern.playground/Contents.swift
+++ builder-pattern.playground/Contents.swift
@@ -1,21 +1,97 @@
-//: A UIKit based Playground for presenting user interface
-  
-import UIKit
 import PlaygroundSupport
+import XCTest
 
-class MyViewController : UIViewController {
-    override func loadView() {
-        let view = UIView()
-        view.backgroundColor = .white
+protocol DomainModel {}
 
-        let label = UILabel()
-        label.frame = CGRect(x: 150, y: 200, width: 200, height: 20)
-        label.text = "Hello World!"
-        label.textColor = .black
-        
-        view.addSubview(label)
-        self.view = view
+struct User: DomainModel {
+    let id: Int
+    let age: Int
+    let email: String
+}
+
+class BaseQueryBuilder<Model: DomainModel> {
+    typealias Predicate = (Model) -> (Bool)
+    
+    func limit(_ limit: Int) -> BaseQueryBuilder<Model> {
+        return self
+    }
+    
+    func filter(_ predicate: @escaping Predicate) -> BaseQueryBuilder<Model> {
+        return self
+    }
+    
+    func fetch() -> [Model] {
+        preconditionFailure("Should be overriden in subclasses")
     }
 }
-// Present the view controller in the Live View window
-PlaygroundPage.current.liveView = MyViewController()
+
+class RealmQueryBuilder<Model: DomainModel>: BaseQueryBuilder<Model> {
+    enum Query {
+        case limit(Int)
+        case filter(Predicate)
+    }
+    
+    fileprivate var operations: [Query] = []
+    
+    @discardableResult
+    override func limit(_ limit: Int) -> BaseQueryBuilder<Model> {
+        operations.append(Query.limit(limit))
+        return self
+    }
+    
+    @discardableResult
+    override func filter(_ predicate: @escaping BaseQueryBuilder<Model>.Predicate) -> BaseQueryBuilder<Model> {
+        operations.append(Query.filter(predicate))
+        return self
+    }
+    
+    override func fetch() -> [Model] {
+        print("RealmQueryBuilder: init with \(operations.count) operations")
+        return RealmProvider().fetch(operations)
+    }
+}
+
+
+class RealmProvider {
+    
+    func fetch<Model: DomainModel>(_ operations: [RealmQueryBuilder<Model>.Query]) -> [Model] {
+        
+        print("RealmProvider: Retrieving data from Realm...")
+        
+        for query in operations {
+            switch query {
+            case .filter(_):
+                print("RealmProvider: some filter operations")
+                break
+                
+            case .limit(_):
+                print("RealmProvider: some limit operations")
+                break
+            }
+        }
+        
+        return []
+    }
+}
+
+
+// Client
+
+class BuilderRealWorld: XCTestCase {
+    
+    func test_builderRealWorld() {
+        print("Client: Start fetching data from realm")
+        
+        let builder = RealmQueryBuilder<User>()
+        let results = builder
+            .filter { user in
+                user.age > 20
+            }
+            .limit(5)
+            .fetch()
+        
+        print("Client: fetched \(results.count) data")
+    }
+}
+
+BuilderRealWorld().test_builderRealWorld()
